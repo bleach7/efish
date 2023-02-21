3a4fefd905b861e2fe3461a942a560e08ac49c68
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-02-21 09:25:34 +0600

    feat: make container

---
+++ components/ui/Container/Container.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { IContainer } from "./Container.interface";
+
+export const Container = ({
+  children,
+  width,
+  className,
+  ...props
+}: IContainer) => {
+  return (
+    <div
+      className={classNames(
+        "mx-auto px-[30px]",
+        {
+          ["max-w-[1260px]"]: width === "landing",
+          ["max-w-[1720px]"]: width === "account",
+          ["max-w-[485px]"]: width === "login",
+        },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
