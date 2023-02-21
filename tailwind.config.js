9acaf74f1b7f021c722c96d5d808391cd44c4eeb
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-02-21 09:09:59 +0600

    style: add base, montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
