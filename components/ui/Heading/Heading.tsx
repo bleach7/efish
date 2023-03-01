ac42ffe1e2e329ff5f63828ef636387e97437888
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-03-01 23:11:41 +0600

    feat: make dynamic tag name for heading

--- components/ui/Heading/Heading.tsx
+++ components/ui/Heading/Heading.tsx
@@ -1,9 +1,11 @@
 import { ArrowLeft } from "@/assets/icons";
 import classNames from "classnames";
 import Link from "next/link";
+import { createElement, Fragment } from "react";
 import { IHeading } from "./Heading.interface";
 
 export const Heading = ({
+  as,
   children,
   size = "sm",
   name,
@@ -13,43 +15,58 @@ export const Heading = ({
   className,
 }: IHeading) => {
   if (path) {
-    return (
-      <div
-        className={classNames(
+    return createElement(
+      "div",
+      {
+        className: classNames(
           "relative mb-[45px] flex items-center gap-x-[19px]",
           className
-        )}
-      >
-        <Link href={path}>
-          <ArrowLeft className="h-auto w-[22px] text-[#52A5FC]" />
-        </Link>
-        <h1
-          className={classNames("font-semibold", {
+        ),
+      },
+      createElement(
+        Link,
+        {
+          href: path,
+        },
+        createElement(ArrowLeft, {
+          className: "h-auto w-[22px] text-[#52A5FC]",
+        })
+      ),
+      createElement(
+        as,
+        {
+          className: classNames("font-semibold", {
             ["text-[18px] leading-[24px]"]: size === "sm",
             ["text-[24px] leading-[32px]"]: size === "md",
             ["text-[36px] leading-[48px]"]: size === "lg",
             ["uppercase"]: uppecase === true,
             ["relative pl-[17px] before:absolute before:top-2/4 before:left-0 before:h-full before:w-[3.6px] before:-translate-y-2/4 before:bg-[#52A5FC] before:content-['']"]:
               line === true,
-          })}
-        >
-          {!name && children}
-          {name && (
-            <>
-              <span>{children}</span>
-              <span className="text-[14px] capitalize leading-[19px] text-[#52A5FC]">
-                {name}
-              </span>
-            </>
-          )}
-        </h1>
-      </div>
+          }),
+        },
+        !name && children,
+        name &&
+          createElement(
+            Fragment,
+            null,
+            createElement("span", null, children),
+            createElement(
+              "span",
+              {
+                className:
+                  "text-[14px] capitalize leading-[19px] text-[#52A5FC]",
+              },
+              name
+            )
+          )
+      )
     );
   }
 
-  return (
-    <div
-      className={classNames(
+  return createElement(
+    as,
+    {
+      className: classNames(
         "font-semibold",
         {
           ["text-[18px] leading-[24px]"]: size === "sm",
@@ -60,17 +77,21 @@ export const Heading = ({
             line === true,
         },
         className
-      )}
-    >
-      {!name && children}
-      {name && (
-        <>
-          <span>{children}</span>
-          <span className="text-[14px] capitalize leading-[19px] text-[#52A5FC]">
-            {name}
-          </span>
-        </>
-      )}
-    </div>
+      ),
+    },
+    !name && children,
+    name &&
+      createElement(
+        Fragment,
+        null,
+        createElement("span", null, children),
+        createElement(
+          "span",
+          {
+            className: "text-[14px] capitalize leading-[19px] text-[#52A5FC]",
+          },
+          name
+        )
+      )
   );
 };
