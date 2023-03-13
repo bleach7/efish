04af7c0b9eab46a25a1613d20d8ca654bd6dbd9f
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-03-13 12:38:28 +0600

    style: add base, montserrat font

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import "@/assets/css/index.css";
+import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
